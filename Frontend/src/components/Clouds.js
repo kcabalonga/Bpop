9367e5a2749fd6381802b1f08e9846a04575af38
kcabalonga <cabalongakirsten@gmail.com>
2024-11-04 14:15:23 -0800

    templates for components

---
+++ Frontend/src/components/Clouds.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Cloud = styled.div`
+    border-radius: 50px;
+    background: #E1E9F5;
+`
+
+const Clouds = () => {
+    return (
+        <Cloud>
+            what are you looking for?
+        </Cloud>
+    )
+}
+
+export default Clouds;
