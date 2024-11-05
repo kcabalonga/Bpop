f6f25405703084bd7b3820917e76b2ee4b6266b2
Kaylee Marquez <marquezkaylee50@gmail.com>
2024-11-04 23:47:54 -0800

    Sign up and Log in html files added

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -19,7 +19,8 @@ db.once('open', () => {
 // Define User Schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  age: Number,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true }
 });
 
