ec7290420233f62c326be5d03a422e79029f6e01
kcabalonga <cabalongakirsten@gmail.com>
2024-11-04 17:47:50 -0800

    centered landing text

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Landing from './components/Landing';
 
 function App() {
 
   return (
     <div className="App" style={{background: '#94ccee',  zIndex: -1}}>
-      <Header/>
+      <Header />
+      <Landing />
     </div>
   );
 }
