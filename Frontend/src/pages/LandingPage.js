da3afd7075194993bbd22946754b70b67fd74e77
Celine <celinechu0905@g.ucla.edu>
2024-11-20 16:52:54 -0800

    connected signin component

--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -1,11 +1,23 @@
 import Header from '../components/Header';
 import Landing from '../components/Landing';
+import SignIn from '../components/signin';
+
+// export default function LandingPage() {
+//     return (
+//         <div>
+//       <Header />
+//       <Landing />
+//       <SignIn/>
+//         </div>
+//     );
+// }
 
 export default function LandingPage() {
-    return (
-        <div>
-      <Header />
-      <Landing />
-        </div>
-    );
+return (
+    <div>
+  <Header />
+  <Landing />
+  <SignIn/>
+    </div>
+);
 }
