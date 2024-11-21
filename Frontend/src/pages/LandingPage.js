2a777454ee1e76b6799a9f66cef5be662d743e40
Celine <celinechu0905@g.ucla.edu>
2024-11-21 00:39:06 -0800

    connect account icon to signin page

--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -1,17 +1,6 @@
 import Header from '../components/Header';
 import Landing from '../components/Landing';
 import SignIn from '../components/signin';
-<<<<<<< HEAD
-
-// export default function LandingPage() {
-//     return (
-//         <div>
-//       <Header />
-//       <Landing />
-//       <SignIn/>
-//         </div>
-//     );
-// }
 
 export default function LandingPage() {
 return (
@@ -21,15 +10,4 @@ return (
   <SignIn/>
     </div>
 );
-=======
-
-export default function LandingPage() {
-    return (
-        <div>
-      <Header />
-      <Landing />
-      <SignIn/>
-        </div>
-    );
->>>>>>> a014582 (updated signin component)
 }
