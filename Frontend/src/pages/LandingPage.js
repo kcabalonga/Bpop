3cf9ff2700eb6804541b66c742cf20a2dfb4e9f9
Kaylee Marquez <marquezkaylee50@gmail.com>
2024-12-02 15:23:02 -0800

    Implemented Header after Sign in to Landingpage

--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -3,18 +3,80 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Background from '../components/Background';
 import Landing from '../components/Landing';
+import Headertwo from '../components/HeaderAfterSignin';
 
 function LandingPage() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const checkLoggedIn = async () => {
+    
+    try {
+
+      const response = await fetch('/api/username');
+
+      if (response.ok) {
+        setIsLoggedIn(true);
+       
+      } 
+      else {
+        setIsLoggedIn(false);
+      }
+
+
+    } catch (error) {
+      console.error("Error checking username", error);
+      alert("An error occurred. Please try again.");
+    }
+  };
+  
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
 
   return (
     <div className="homepage">
-    <Header />
-    <Background>
-      <Landing />
-    </Background>
+      {isLoggedIn ? <Headertwo /> : <Header />}
+      <Background>
+        <Landing />
+      </Background>
     </div>
   );
+
+
 }
 
+
 export default LandingPage;
+
+
+
+
+
+//   checkLoggedIn();
+
+
+// if (isLoggedIn === false){
+//   return (
+//     <div className="homepage">
+//     <Header />
+//     <Background>
+//       <Landing />
+//     </Background>
+//     </div>
+//   );
+// }
+
+// else {
+
+//   return (
+//     <div className="homepage">
+//     <Headertwo />
+//     <Background>
+//       <Landing />
+//     </Background>
+//     </div>
+//   );
+
+// }
+// }
